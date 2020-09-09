7ae709d2b72728ff973d1c0d2d233e30e5959cb6
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2020-09-09 15:30:21 +0530

    Revert "Merge pull request #1 from web-tech-sem3/arvind"
    This reverts commit fc6e5da5d9e0e070606fa9e9f9b462b0a77db500.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "fontsource-roboto";
-import * as serviceWorker from "./serviceWorker";
-import Table from "./components/SignUpForm";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'fontsource-roboto';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(< Table />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
