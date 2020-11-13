2c08b4beb469999c5bb6a53ed689af81243befe1
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-11-13 21:50:17 +0530

    rating

--- frontend/src/components/rating.js
+++ frontend/src/components/rating.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import RatingStar from './ratingStar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 275,
+    paddingTop: 30,
+    height: 100,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    alignContent: 'center',
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const Rating = () => {
+  const classes = useStyles();
   return (
-    <Card>
-      <RatingStar />
-    </Card>
+    <div align="center">
+      <Card className={classes.root}>
+        <RatingStar />
+      </Card>
+    </div>
   );
 };
 
