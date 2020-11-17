22baa588383923b252e1637d4cfd43a9eac67643
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-11-17 20:58:17 +0530

    rating

--- frontend/src/components/rating.js
+++ frontend/src/components/rating.js
@@ -4,6 +4,7 @@ import RatingStar from './ratingStar';
 import { makeStyles } from '@material-ui/core/styles';
 import Copyright from '../components/copyright';
 import background from '../images/background.jpg';
+import { CircularProgress } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -13,14 +14,14 @@ const useStyles = makeStyles({
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     alignContent: 'center',
     align: 'center',
-    transition: 'all .25s linear',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },
   pos: {
     marginBottom: 12,
   },
   copyright: {
-    height: 100,
+    paddingTop: 50,
+    height: 90,
     alignContent: 'center',
     position: 'bottom',
   },
@@ -29,41 +30,45 @@ const useStyles = makeStyles({
 const Rating = () => {
   const classes = useStyles();
   return (
-    <div
-      align="center"
-      style={{
-        backgroundImage: `url(${background})`,
-        height: '83vh',
-        backgroundSize: 'cover',
-        position: 'relative',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      <br />
-      <br />
-      <br />
-      <Card className={classes.root}>
-        Customer Care
-        <RatingStar num={1} />
-      </Card>
-      <br />
-      <Card className={classes.root}>
-        Caring
-        <RatingStar num={2} />
-      </Card>
-      <br />
-      <Card className={classes.root}>
-        UX
-        <RatingStar num={3} />
-      </Card>
-      <br />
-      <Card className={classes.root}>
-        UI
-        <RatingStar num={4} />
-      </Card>
-      <br />
-      <br />
+    <div align="center">
+      <div
+        align="center"
+        style={{
+          backgroundImage: `url(${background})`,
+          height: '83vh',
+          backgroundSize: 'cover',
+          paddingLeft: 700,
+          position: 'relative',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <br />
+        <br />
+        <br />
+        <Card className={classes.root}>
+          Customer Care
+          <RatingStar num={1} />
+        </Card>
+        <br />
+        <Card className={classes.root}>
+          Caring
+          <RatingStar num={2} />
+        </Card>
+        <br />
+        <Card className={classes.root}>
+          UX
+          <RatingStar num={3} />
+        </Card>
+        <br />
+        <Card className={classes.root}>
+          UI
+          <RatingStar num={4} />
+        </Card>
+        <br />
+        <br />
+        <br />
+      </div>
       <br />
       <Copyright />
     </div>
