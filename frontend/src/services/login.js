ac1bb4c0234c69079de07f7af356ba523d4bc2ac
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-11-20 22:27:22 +0530

    login logs

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 const baseURL = 'http://localhost:3001/api/login';
 
 const login = async object => {
-  console.log('inside');
   const res = await axios.post(baseURL, object);
-  console.log(res.data);
   return res.data;
 };
 
