dfdd061235bff2b3951c56ed5abe36a44b50aef4
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-11-23 11:14:08 +0530

    backdrops

--- frontend/src/components/workProgress.js
+++ frontend/src/components/workProgress.js
@@ -1,4 +1,11 @@
-import { Button, makeStyles, Stepper, TextField } from '@material-ui/core';
+import {
+  Backdrop,
+  Button,
+  CircularProgress,
+  makeStyles,
+  Stepper,
+  TextField,
+} from '@material-ui/core';
 import {
   Timeline,
   TimelineConnector,
@@ -10,22 +17,34 @@ import {
 import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   dot: {
     transition: 'transform .3s',
+    width: '4px',
+    height: '4px',
     '&:hover': {
       transform: 'scale(3)',
+      background: 'lightpink',
     },
   },
-});
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 const WorkProgress = () => {
   const [task, setTask] = useState(null);
+  const [backdrop, setBackdrop] = useState(false);
   const classes = useStyles();
 
   const handleTaskSubmit = e => {
     e.preventDefault();
     setTask(null);
+  };
+
+  const handleBackdropOpen = () => {
+    setBackdrop(true);
   };
 
   const handleTaskChange = e => {
@@ -35,42 +54,50 @@ const WorkProgress = () => {
 
   return (
     <div>
-      <Form onSubmit={handleTaskSubmit}>
-        <TextField
-          variant="outlined"
-          label="Task Done"
-          value={task}
-          onChange={handleTaskChange}
-        />
-        <Button color="secondary" type="submit">
-          Add
-        </Button>
-      </Form>
+      <Backdrop
+        open={backdrop}
+        className={classes.backdrop}
+        onClick={() => {
+          setBackdrop(!backdrop);
+        }}
+      >
+        <Form onSubmit={handleTaskSubmit}>
+          <TextField
+            variant="outlined"
+            label="Task Done"
+            value={task}
+            onChange={handleTaskChange}
+          />
+          <Button color="secondary" type="submit">
+            Add
+          </Button>
+        </Form>
+      </Backdrop>
       <Timeline>
         <TimelineItem>
           <TimelineSeparator>
-            <TimelineDot className={classes.dot} />
+            <TimelineDot className={classes.dot} onClick={handleBackdropOpen} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>Eat</TimelineContent>
         </TimelineItem>
         <TimelineItem>
           <TimelineSeparator>
-            <TimelineDot className={classes.dot} />
+            <TimelineDot className={classes.dot} onClick={handleBackdropOpen} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>Code</TimelineContent>
         </TimelineItem>
         <TimelineItem>
           <TimelineSeparator>
-            <TimelineDot className={classes.dot} />
+            <TimelineDot className={classes.dot} onClick={handleBackdropOpen} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>Sleep</TimelineContent>
         </TimelineItem>
         <TimelineItem>
           <TimelineSeparator>
-            <TimelineDot className={classes.dot} />
+            <TimelineDot className={classes.dot} onClick={handleBackdropOpen} />
           </TimelineSeparator>
           <TimelineContent>Repeat</TimelineContent>
         </TimelineItem>
