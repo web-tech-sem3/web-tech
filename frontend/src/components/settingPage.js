96643f40c4b6945adf4d33bf37324b5270a523e1
Tanmay Gairola <tanmaygairola12@gmail.com>
2020-12-07 11:20:16 +0530

    settings

--- frontend/src/components/settingPage.js
+++ frontend/src/components/settingPage.js
@@ -1,5 +1,6 @@
 import { Avatar, Divider, makeStyles } from '@material-ui/core';
 import React from 'react';
+import arvind from '../images/arvind.jpg';
 import background from '../images/background.jpg';
 import SettingsAccordion from './settingsAccordion';
 
@@ -7,7 +8,13 @@ const useStyles = makeStyles({
   avatar: {
     width: '50vh',
     height: '50vh',
+    borderRadius: '100%',
     margin: '7vh',
+    transition: 'transform .3s ease-in-out',
+    '&:hover': {
+      boxShadow: '1px 1px 20px grey',
+      transform: 'scale(1.06)',
+    },
   },
   accordion: {
     width: '100%',
@@ -31,7 +38,7 @@ const SettingsPage = () => {
         display: 'flex',
       }}
     >
-      <Avatar className={classes.avatar} />
+      <Avatar className={classes.avatar} src={arvind} />
       <Divider orientation="vertical" style={{ width: '1vh' }} />
 
       <div style={{ paddingLeft: '5vh' }} className={classes.accordion}>
